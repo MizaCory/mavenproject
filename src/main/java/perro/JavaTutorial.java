b2a460edfc2a26db3bf6ee47a9c4edee392aa744
MizaCory <mizacory@GMAIL.COM>
2022-11-16 21:01:42 -0600

    Update JavaTutorial.java
    we create a function that calculate the quote

--- src/main/java/perro/JavaTutorial.java
+++ src/main/java/perro/JavaTutorial.java
@@ -1,5 +1,19 @@
 package perro;
+
 public class JavaTutorial{
+		static int cost=1200, cost2=1300;
 	public static void main(String args[]){
-		System.out.println("Hello World");}
+		String c="obet";
+		System.out.println(cost);
+		System.out.println(c);
+		System.out.println(cost2);
+		System.out.println(cotizacion(1, 1));
+		System.out.println(cotizacion(2, 3));
+	}
+	static int cotizacion (int num1, int num2){
+		return (num1*cost)+(num2*cost2);
+	}
+
+
+
 }
